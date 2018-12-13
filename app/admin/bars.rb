f80c00d2e39950f5bd975af2b08ab8b0e0e8b1b1
Raghu Betina <raghu@firstdraft.com>
2018-12-13 04:28:27 +0000

    Whitelist columns in Bar for ActiveAdmin

--- app/admin/bars.rb
+++ app/admin/bars.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Bar do
+
+ permit_params :name, :location, :drink_deal
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
