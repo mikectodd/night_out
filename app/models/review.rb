cd4e6b873bf1fc2aef72e2d53d7e6ad6be684f78
Raghu Betina <raghu@firstdraft.com>
2018-12-13 04:27:18 +0000

    Validate presence of reviews.body

--- app/models/review.rb
+++ app/models/review.rb
@@ -5,6 +5,8 @@ class Review < ApplicationRecord
 
   # Validations
 
+  validates :body, :presence => true
+
   validates :restaurant_id, :presence => true
 
   validates :reviewer_id, :presence => true
