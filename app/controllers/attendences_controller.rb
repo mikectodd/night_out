a9279651208eaa4bcc194968dffa7613ee3fa32d
Raghu Betina <raghu@firstdraft.com>
2018-12-13 04:29:08 +0000

    Only attendence user can access show page

--- app/controllers/attendences_controller.rb
+++ app/controllers/attendences_controller.rb
@@ -1,5 +1,5 @@
 class AttendencesController < ApplicationController
-  before_action :current_user_must_be_attendence_user, :only => [:edit_form, :update_row, :destroy_row]
+  before_action :current_user_must_be_attendence_user, :only => [:show, :edit_form, :update_row, :destroy_row]
 
   def current_user_must_be_attendence_user
     attendence = Attendence.find(params["id_to_display"] || params["prefill_with_id"] || params["id_to_modify"] || params["id_to_remove"])
