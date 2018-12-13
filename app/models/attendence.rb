1f0a2f8bac52e0d734c5ebf4f6686a2e04ad0dd3
Raghu Betina <raghu@firstdraft.com>
2018-12-13 04:27:18 +0000

    Validate uniqueness of attendences.restaurant_id

--- app/models/attendence.rb
+++ app/models/attendence.rb
@@ -5,6 +5,8 @@ class Attendence < ApplicationRecord
 
   # Validations
 
+  validates :restaurant_id, :uniqueness => { :scope => [:user_id], :message => "already attending" }
+
   validates :restaurant_id, :presence => true
 
   validates :user_id, :presence => true
