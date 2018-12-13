1a5656cb9bf5d7a53deda362f4f76f69d62f436f
Raghu Betina <raghu@firstdraft.com>
2018-12-13 04:29:09 +0000

    Automatically associate friend_request to signed in user

--- app/controllers/friend_requests_controller.rb
+++ app/controllers/friend_requests_controller.rb
@@ -41,7 +41,7 @@ class FriendRequestsController < ApplicationController
   def update_row
     @friend_request = FriendRequest.find(params.fetch("id_to_modify"))
 
-    @friend_request.sender_id = params.fetch("sender_id")
+    
     @friend_request.recipient_id = params.fetch("recipient_id")
 
     if @friend_request.valid?
